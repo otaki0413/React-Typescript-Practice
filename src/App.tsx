e6be5679fea6bd80e91670ff1c7e4c3be53e931a
otaki0413 <67969654+otaki0413@users.noreply.github.com>
2022-10-15 17:01:55 +0900

    APIから取得したデータの型を定義する

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import axios from 'axios';
+import { useState } from 'react';
 import './styles.css';
+import { Todo } from './Todo';
+
+// APIから取得するデータの型を設定
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get Data</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId} />
+      ))}
     </div>
   );
 }
